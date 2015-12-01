7b2445f4bf598c73ab896bf66670df34f5d2b041
caracolchichi <andres.diaz@unitec.edu>
2015-11-30 19:43:54 -0600

    Searchbar

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -2,4 +2,11 @@ Template.MasterLayout.helpers({
 });
 
 Template.MasterLayout.events({
+
+});
+
+Template.MasterLayout.onRendered(function () {
+
+  $(".button-collapse").sideNav();
+  $('.modal-trigger').leanModal();
 });
