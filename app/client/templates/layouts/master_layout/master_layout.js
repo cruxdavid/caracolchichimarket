ebeec28f4676c55128afb2bfd7f0bb8410686ef9
caracolchichi <andres.diaz@unitec.edu>
2015-12-05 00:50:54 -0600

    Login y logout

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -2,11 +2,41 @@ Template.MasterLayout.helpers({
 });
 
 Template.MasterLayout.events({
-
+  'click .logout': function(event){
+       event.preventDefault();
+       Meteor.logout();
+       window.location.reload();
+   }
 });
 
 Template.MasterLayout.onRendered(function () {
 
   $(".button-collapse").sideNav();
   $('.modal-trigger').leanModal();
+});
+
+Template.loginModalContent.events({
+  'submit form': function(event){
+       event.preventDefault();
+       var email = $('[name=email]').val();
+       var password = $('[name=password]').val();
+       Meteor.loginWithPassword(email, password);
+       console.log("Form submitted.");
+       Router.go('home');
+  }
+});
+
+Template.registerModalContent.events({
+  'submit form': function(event){
+       event.preventDefault();
+       var emailvar = event.target.email.value;
+       var passwordvar = event.target.password.value;
+       Accounts.createUser({
+           email: emailvar,
+           password: passwordvar
+       });
+       Router.go('home');
+       console.log("Form submitted.");
+   }
+
 });
